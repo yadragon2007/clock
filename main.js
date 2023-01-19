71e094ecb6a330e56aad6dbe8c2e4284645fe19c
yousef amr <102539850+yadragon2007@users.noreply.github.com>
2023-01-19 20:56:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+let lm = document.getElementById("m");
+let lh = document.getElementById("h");
+let ls = document.getElementById("s");
+let i = 0;
+function run() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+
+  ls.style.transition = `.5s`;
+  ls.style.transform = `translate(-50%, -50%) rotate(${s * 6}deg)`;
+  lm.style.transform = `translate(-50%, -50%) rotate(${m * 6}deg)`;
+
+  if (s == 59) {
+    ls.style.transform = `translate(-50%, -50%) rotate(360deg)`;
+    setTimeout(() => {
+      ls.style.transition = `0s`;
+      ls.style.transform = `translate(-50%, -50%) rotate(0deg)`;
+    }, 500);
+    run;
+  }
+  if (h >= 12) {
+    h = h - 12;
+  }
+  lh.style.transform = `translate(-50%, -50%) rotate(${h * 30 + m / 2}deg)`;
+
+  setTimeout(run, 1000);
+}
+run();
