604a7299ebc73d51bb636d1e08b46a38a016483c
yousef amr <102539850+yadragon2007@users.noreply.github.com>
2023-12-06 21:22:13 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 let lm = document.getElementById("m");
 let lh = document.getElementById("h");
 let ls = document.getElementById("s");
+let p = document.querySelector("p");
+let span = document.querySelector("span");
 let i = 0;
+
 function run() {
   const today = new Date();
   let h = today.getHours();
   let m = today.getMinutes();
   let s = today.getSeconds();
+  let d = today.getDate();
+  let mo = today.getMonth();
+  let y = today.getFullYear();
 
   ls.style.transition = `.5s`;
   ls.style.transform = `translate(-50%, -50%) rotate(${s * 6}deg)`;
-  lm.style.transform = `translate(-50%, -50%) rotate(${m * 6}deg)`;
+  lm.style.transform = `translate(-50%, -50%) rotate(${m * 6 + s / 10}deg)`;
 
   if (s == 59) {
     ls.style.transform = `translate(-50%, -50%) rotate(360deg)`;
@@ -20,11 +26,24 @@ function run() {
     }, 500);
     run;
   }
+  if (m == 59) {
+    lm.style.transform = `translate(-50%, -50%) rotate(360deg)`;
+    setTimeout(() => {
+      lm.style.transition = `0s`;
+      lm.style.transform = `translate(-50%, -50%) rotate(0deg)`;
+    }, 500);
+    run;
+  }
   if (h >= 12) {
     h = h - 12;
   }
   lh.style.transform = `translate(-50%, -50%) rotate(${h * 30 + m / 2}deg)`;
 
+  p.innerHTML = `${y}/${mo + 1}/${d}`;
+
+
+
   setTimeout(run, 1000);
 }
-run();
+
+run()
